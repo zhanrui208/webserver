47f90e537949b0391ad525a3011bdb45f8b40de6
rui_zhan <rui_zhan@kingdee.com>
2016-03-22 13:42:03 +0800

    修改发生的域名

--- src/com/web/base/Constant.java
+++ src/com/web/base/Constant.java
@@ -2,6 +2,6 @@ package com.web.base;
 
 public class Constant {
 	public static String UTF_8 ="UTF-8";
-	public static String ACTIVEURL= "http://localhost:8080/webserver/activateUser";
-	public static String UPDATEPWD= "http://localhost:8080/webserver/rest/updatepwd";
+	public static String ACTIVEURL= "http://qhongmeet.com/webserver/activateUser";
+	public static String UPDATEPWD= "http://qhongmeet.com/webserver/rest/updatepwd";
 }
